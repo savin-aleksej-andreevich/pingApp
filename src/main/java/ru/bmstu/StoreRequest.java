43ff6d96b8d1c8a77379e443978f394e88ebc352
Alexey Savin <therealsava@icloud.com>
2021-12-24 17:53:26 +0300

    Scripted auto-commit on change (2021-12-24 17:53:26) by gitwatch.sh

--- src/main/java/ru/bmstu/StoreRequest.java
+++ src/main/java/ru/bmstu/StoreRequest.java
@@ -1,2 +1,19 @@
-package ru.bmstu;public class StoreRequest {
+package ru.bmstu;
+
+public class StoreRequest {
+    private final String url;
+    private final Long time;
+
+    public StoreRequest(String url, Long time) {
+        this.url = url;
+        this.time = time;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Long getTime() {
+        return time;
+    }
 }
