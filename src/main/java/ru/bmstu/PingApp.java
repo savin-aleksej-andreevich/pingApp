0c1049643ee714b593f5f8604ebe6f35ebadbfbd
Alexey Savin <therealsava@icloud.com>
2021-12-24 20:15:08 +0300

    Scripted auto-commit on change (2021-12-24 20:15:08) by gitwatch.sh

--- src/main/java/ru/bmstu/PingApp.java
+++ src/main/java/ru/bmstu/PingApp.java
@@ -18,8 +18,7 @@ import akka.stream.javadsl.Keep;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 
-import static org.asynchttpclient.Dsl.asyncHttpClient;
-import org.asynchttpclient.AsyncHttpClient;
+
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Collections;
