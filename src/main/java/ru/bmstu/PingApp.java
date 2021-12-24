494b24e9fdeebf09dacff22605a166c4ef858a61
Alexey Savin <therealsava@icloud.com>
2021-12-24 19:41:53 +0300

    Scripted auto-commit on change (2021-12-24 19:41:53) by gitwatch.sh

--- src/main/java/ru/bmstu/PingApp.java
+++ src/main/java/ru/bmstu/PingApp.java
@@ -4,7 +4,15 @@ import akka.actor.ActorRef;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.Query;
 import akka.japi.Pair;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Keep;
+import akka.stream.javadsl.Source;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
 
 public class PingApp {
     public static final int SERVER_PORT = 8080;
@@ -18,5 +26,33 @@ public class PingApp {
         String url = query.get("testURL").orElse(DEFAULT_URL);
         String count = query.get("count").orElse("10");
         return new Pair<String, Integer>(url, Integer.parseInt(count));
+    }
+
+    private static CompeletionStage <Object> asy (Pair<String, Integer> pair) {
+        return Patterns.ask(cache, pair, Duration.ofMillis(5000)).thenCompose(
+                result-> {
+                    long responceTime = ((CacheResponse) result).getTime();
+                    if (responceTime > 0) {
+                        return CompletableFuture.completedFuture(new Pair<>(pair.first(), responceTime));
+                    }
+                    return Source.from(Collections.singletonList(pair))
+                            .toMat(
+                                    testSink(),
+                                    Keep.right()
+                            )
+                            .run(materializer)
+                            .thenCompose( sum ->
+                                    CompletableFuture.completedFuture(
+                                            new Pair<>(pair.first(), sum / pair.second())
+                                    )
+
+                            );
+                });
+    }
+
+    private static Sink<String, Integer>, CompetionStage<Long>> testSink() {
+        return Flow
+                .<Pair<String, Integer>>create()
+                .mapConcat(request -> Collections.nCopies(request.second(), request.first()))
     }
 }
