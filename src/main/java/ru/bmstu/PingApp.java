1c6f8bcf9e4060b2b0d14f9b1729e00072db95b6
Alexey Savin <therealsava@icloud.com>
2021-12-24 19:22:04 +0300

    Scripted auto-commit on change (2021-12-24 19:22:04) by gitwatch.sh

--- src/main/java/ru/bmstu/PingApp.java
+++ src/main/java/ru/bmstu/PingApp.java
@@ -1,2 +1,21 @@
-package ru.bmstu;public class PingApp {
+package ru.bmstu;
+
+import akka.actor.ActorRef;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.Query;
+import akka.stream.ActorMaterializer;
+
+public class PingApp {
+    public static final int SERVER_PORT = 8080;
+    public static final int OK_CODE = 200;
+    public static ActorRef cache;
+    public static ActorMaterializer materializer;
+    public static final String DEFAULT_URL = "https://google.ru/";
+
+    public static Pair<String, Integer> makePair(HttpRequest request) {
+        Query query = request.getUri().query();
+        String url = query.get("testURL").orElse(DEFAULT_URL);
+        String count = query.get("count").orElse("10");
+        return new Pair<String, Integer>(url, Integer.parseInt(count));
+    }
 }
