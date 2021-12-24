dc1c1a5c9a134fc2201845496cdb129c8ecda663
Alexey Savin <therealsava@icloud.com>
2021-12-24 17:51:50 +0300

    Scripted auto-commit on change (2021-12-24 17:51:50) by gitwatch.sh

--- src/main/java/ru/bmstu/CacheResponse.java
+++ src/main/java/ru/bmstu/CacheResponse.java
@@ -3,4 +3,9 @@ package ru.bmstu;
 public class CacheResponse {
     private final String url;
     private final Long time;
+
+    public CacheResponse (String url, Long time) {
+        this.url = url;
+        this.time = time;
+    }
 }
